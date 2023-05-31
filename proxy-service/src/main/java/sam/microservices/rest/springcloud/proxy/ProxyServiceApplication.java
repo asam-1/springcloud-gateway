e93a8166af27cd8c4384add293f32778a7583c04
asam-1 <bensam201201@gmail.com>
2023-05-31 12:06:28 +0200

    Eureka Discovery service with a dynamic routing for the gateway-service

--- proxy-service/src/main/java/sam/microservices/rest/springcloud/proxy/ProxyServiceApplication.java
+++ proxy-service/src/main/java/sam/microservices/rest/springcloud/proxy/ProxyServiceApplication.java
@@ -10,24 +10,18 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+
 public class ProxyServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProxyServiceApplication.class, args);
     }
 
+    
     @Bean
-    RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
-        return builder.routes()
-                .route(r -> r.path("/products/**")
-                        .filters(f -> f.addRequestHeader("custom-product-request-header", "custom-product-request-header")
-                                .addResponseHeader("custom-product-response-header", "custom-product-response-header"))
-                        .uri("lb://products-service/"))
-                .route(r -> r.path("/customers/**")
-                        .filters(f -> f.addRequestHeader("custom-customer-request-header", "custom-customer-request-header")
-                                .addResponseHeader("custom-customer-response-header", "custom-customer-response-header"))
-                        .uri("lb://customers-service/"))
-                .build();
+    DiscoveryClientRouteDefinitionLocator dynamicRoutes(ReactiveDiscoveryClient reactiveDiscoveryClient,
+                                                        DiscoveryLocatorProperties properties) {
+        return new DiscoveryClientRouteDefinitionLocator(reactiveDiscoveryClient, properties);
     }
 
 }
