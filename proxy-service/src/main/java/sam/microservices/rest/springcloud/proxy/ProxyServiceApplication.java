d819cc7d24fe9aa56afa3598e9635ddeca674623
asam-1 <bensam201201@gmail.com>
2023-05-31 10:38:08 +0200

    Eureka Discovery service with a static java based configuration for the gateway service

--- proxy-service/src/main/java/sam/microservices/rest/springcloud/proxy/ProxyServiceApplication.java
+++ proxy-service/src/main/java/sam/microservices/rest/springcloud/proxy/ProxyServiceApplication.java
@@ -16,18 +16,18 @@ public class ProxyServiceApplication {
         SpringApplication.run(ProxyServiceApplication.class, args);
     }
 
- /*   @Bean
+    @Bean
     RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route(r -> r.path("/products/**")
                         .filters(f -> f.addRequestHeader("custom-product-request-header", "custom-product-request-header")
                                 .addResponseHeader("custom-product-response-header", "custom-product-response-header"))
-                        .uri("http://localhost:8082/"))
+                        .uri("lb://products-service/"))
                 .route(r -> r.path("/customers/**")
                         .filters(f -> f.addRequestHeader("custom-customer-request-header", "custom-customer-request-header")
                                 .addResponseHeader("custom-customer-response-header", "custom-customer-response-header"))
-                        .uri("http://localhost:8081/"))
+                        .uri("lb://customers-service/"))
                 .build();
-    }*/
+    }
 
 }
